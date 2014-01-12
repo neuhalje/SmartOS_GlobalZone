c5ac2746a001faa6e62fcd4e3eb3731e051c6f5d
Jens Neuhalfen <jens@neuhalfen.name>
2014-01-12 22:04:14 +0100

    ~/.ssh/authorized_keys is not used by SmartOS; copy to /usbkey/config.inc/authorized_keys

--- ssh_authorized_keys/setup_ssh/copy-in-ssh-key.sh
+++ ssh_authorized_keys/setup_ssh/copy-in-ssh-key.sh
@@ -3,8 +3,20 @@
 SSH_DIR=/opt/custom/setup_ssh/root.ssh
 if [ -d $SSH_DIR ]
    then
+  # ~/.ssh/authorized_keys is not used by SmartOS
    mkdir -p /root/.ssh && cp $SSH_DIR/* /root/.ssh/ && chmod 600 /root/.ssh/* && chmod 700 /root/.ssh
 
+fi
+
+
+grep -q root_authorized_keys_file /usbkey/config
+
+if [ $? != 0 ]
+then
+  echo root_authorized_keys_file=authorized_keys>> /usbkey/config
+
+  mkdir /usbkey/config.inc
+  cp $SSH_DIR/authorized_keys /usbkey/config.inc/authorized_keys
 fi
 
 exit 0
