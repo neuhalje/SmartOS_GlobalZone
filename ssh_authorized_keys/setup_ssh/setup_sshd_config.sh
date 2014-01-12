890a8eb7154be0c6d03aa731350c2b5905872cbc
Jens Neuhalfen <jens@neuhalfen.name>
2014-01-12 22:01:04 +0100

    Fixed varibale name typo

--- ssh_authorized_keys/setup_ssh/setup_sshd_config.sh
+++ ssh_authorized_keys/setup_ssh/setup_sshd_config.sh
@@ -4,7 +4,7 @@ SSHD_SRC_DIR=/opt/custom/setup_ssh/sshd
 SSHD_DST_DIR=/usbkey/ssh
 if [ -d $SSHD_SRC_DIR ]
 then
-   cp $SSH_SRC_DIR/* $SSHD_DST_DIR/. && chmod 600 $SSHD_DST_DIR/* && chmod 700 $SSHD_DST_DIR
+   cp $SSHD_SRC_DIR/* $SSHD_DST_DIR/. && chmod 600 $SSHD_DST_DIR/* && chmod 700 $SSHD_DST_DIR
 fi
 
 exit 0
